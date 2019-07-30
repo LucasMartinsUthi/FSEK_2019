49c1c7455b86d7424270d1d52b27896ee2d76695
LucasMartinsUthi <luscagbr@gmail.com>
2019-07-30 14:46:59 -0300

    Estrutura básica do código

---
+++ main/classRobot.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# so that script can be run from Brickman
+
+# from ev3dev.ev3 import *
+import math
+from time import sleep
+
+class Robot:
+    def __init__(self, **kwargs):
+        self.radius = kwargs.get('radius')
+        self.name = kwargs.get('name')
+        self.state = kwargs.get('state')
+
+    def debug(self):
+        for key, arg in self.__dict__.items(): 
+            print key, "-", arg
+
+    def odemetry(self):
+        #calcular odometria do robô
+
+    def moveCm(self):
+        #calcular odometria do robô
+
+    def rotateDegrees(self):
+        #calcular odometria do robô
+
+    def getSensor(self):
+        #retornar valores dos sensores já tratados
+        #pode chamar outras funções como hsv
+
+    def hsv(self):
+
+    def closeClaw(self):
+
+    def openClaw(self):
+
+    def stop(self):
+
+    def preventNoise(self):
+
+    def learnHolePositions(self):
+
+    def learnTubesPositions(self):
+
+
+
+
+
